ce5b8278d9a8cb0445bc422771b05c1253c4139f
slide1020 <slide1020@naver.com>
2024-05-24 15:39:49 +0900

    Failed

--- LeetCode/src/Ransom_Note/RN_Solution_day02.java
+++ LeetCode/src/Ransom_Note/RN_Solution_day02.java
@@ -10,8 +10,8 @@ public class RN_Solution_day02 {
 		String magazine2 = "ab";
 		String ransomNote3 = "aa";
 		String magazine3 = "aab";
-		String ransomNote4 = "aab";
-		String magazine4 = "baa";
+		String ransomNote4 = "aa";
+		String magazine4 = "ab";
 		
 		canConstruct(ransomNote4, magazine4);
 	}
@@ -19,22 +19,33 @@ public class RN_Solution_day02 {
     public static boolean canConstruct(String ransomNote, String magazine) {
     	HashMap<Integer, String> hm = new HashMap<Integer, String>();
     	String putWork = "";
-    	int Idx = 0;
+    	String findWork = "";
+    	int rnCnt = 0;
+    	boolean rnBool = false;
     	
     	for(int i=1; i<=magazine.length();i++) {
     		putWork = magazine.substring(i-1,i);
     		hm.put(i, putWork);
     	}
-    	
-       	for(int i=1; i<=magazine.length();i++) {
-       		if(ransomNote.substring(i-1,i).equals(hm.get(i))) {
-       			
+
+       	for(int j=1; j<=ransomNote.length();j++) {
+       		findWork = ransomNote.substring(j-1,j);
+       		
+       		for(int k=1; k<=hm.size(); k++) {
+       			if(findWork.equals(hm.get(k))){
+       				rnCnt++;
+       				rnBool = true;
+       			}
        		}
     	}
+       	
+       	
+       	if(rnBool && rnCnt == ransomNote.length()) {
+       		System.out.println("true");
+       		return true;
+       	}
     	
-    	System.out.println(hm);
-    	
-    	return true;
+       	System.out.println("false");
+    	return false;
     }
-
 }
